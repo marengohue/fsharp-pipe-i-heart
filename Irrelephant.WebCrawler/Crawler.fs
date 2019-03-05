8b01eb3b29a189591bea3a848f3fb7f7aaf980bb
Eugene Rebedailo <mokonamodokidesu@gmail.com>
2019-03-05 18:24:23 +0300

    Initial

---
+++ Irrelephant.WebCrawler/Crawler.fs
@@ -0,0 +1,42 @@
+﻿module Crawler
+
+    open System
+    open Http
+    open System.Net.Http
+
+    type Link = Link of Uri
+
+    type Logger = Logger of (string -> string -> unit)
+
+    type Webpage =
+        | CrawledWebpage of Link list * Link
+        | UncrawledWebpage of Link
+
+    type Crawler = {
+        pages: Webpage list
+        logger: Logger
+    }
+
+    let makeLink url = 
+        match Uri.TryCreate(url, UriKind.RelativeOrAbsolute) with 
+            | true, uri -> Some (Link uri)
+            | _ -> None
+
+    let getAllLinks httpClient (Link linkUrl) =
+        let allWebpageContent = linkUrl
+            |> fetchUsingClient httpClient
+            |> extractLinks
+
+        match makeLink "ds" with
+            | Some link -> [ link ]
+            | None -> [ ]
+
+    let crawlSinglePage httpClient webpage =
+        match webpage with
+            | UncrawledWebpage pageLink -> [ CrawledWebpage (pageLink |> (getAllLinks httpClient), pageLink)]
+            | _ -> [ webpage ]
+
+    let crawlWebsite (crawler: Crawler) =
+        use client =  new HttpClient()
+        let newPages = crawler.pages |> List.collect (crawlSinglePage client)
+        { crawler with pages = newPages }
---
+++ Irrelephant.WebCrawler/Http.fs
@@ -0,0 +1,9 @@
+﻿module Http
+
+    open System.Net.Http
+
+    let fetchUsingClient (client:HttpClient) (url: string) =
+        client.GetStringAsync url
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
